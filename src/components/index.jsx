75fd4e62072ed580235c04d7e4294154d5043758
Grace <graceofubu04@gmail.com>
2024-08-03 18:53:53 +0100

    Built navbar and sidebar

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,6 @@
 export { default as Button } from './Button';
 export { default as ThemeSettings } from './ThemeSettings';
 export { default as SideBar } from './SideBar';
-// eslint-disable-next-line import/no-cycle
 export { default as Navbar } from './Navbar';
 export { default as Footer } from './Footer';
 export { default as Cart } from './Cart';
