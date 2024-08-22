231c2becac703f76c267151ab2090dc845005585
Grace <graceofubu04@gmail.com>
2024-08-22 11:59:07 +0100

    orders page done

--- src/App.js
+++ src/App.js
@@ -25,18 +25,25 @@ import {
 import { useStateContext } from "./contexts/contextProvider";
 
 function App() {
-  const { activeMenu } = useStateContext();
+  const {
+    activeMenu,
+    themeSettings,
+    setThemeSettings,
+    currentColor,
+    currentMode
+  } = useStateContext();
   return (
-    <div>
+    <div className={currentMode === 'Dark' ? 'dark' : ''}>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
             <TooltipComponent content="Settings" position="TopCenter">
               <button
+                onClick={() => setThemeSettings(true)}
                 type="button"
                 className="text-3xl p-3 hover:drop-shadow-xl
               hover:bg-light-gray text-white"
-                style={{ background: "blue", borderRadius: "50%" }}
+                style={{ background: currentColor, borderRadius: "50%" }}
               >
                 <FiSettings />
               </button>
@@ -52,7 +59,7 @@ function App() {
             </div>
           )}
           <div
-            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full
+            className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full
               ${activeMenu ? "md:ml-72" : "flex-2"}`}
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
@@ -60,6 +67,7 @@ function App() {
             </div>
 
             <div>
+              {themeSettings && <ThemeSettings />}
               <Routes>
                 {/*Dashboard */}
                 <Route path="/" element={<Ecommerce />} />
@@ -74,7 +82,7 @@ function App() {
                 <Route path="/kanban" element={<Kanban />} />
                 <Route path="/editor" element={<Editor />} />
                 <Route path="/calendar" element={<Calendar />} />
-                <Route path="/colour-picker" element={<ColourPicker />} />
+                <Route path="/color-picker" element={<ColourPicker />} />
 
                 {/*Charts */}
                 <Route path="/line" element={<Line />} />
--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -6,6 +6,7 @@ import { earningData, SparklineAreaData, ecomPieChartData } from '../data/dummy'
 import { useStateContext } from '../contexts/contextProvider'
 
 function Ecommerce() {
+  const { currentColor } = useStateContext()
   return (
     <div className='mt-12'>
       <div className='flex flex-wrap lg:flex-nowrap justify-center'>
@@ -20,7 +21,7 @@ function Ecommerce() {
           <div className='mt-6'>
             <Button
               color='white'
-              bgColor='blue'
+              bgColor={currentColor}
               text="Download"
               borderRadius='10px'
               size='md'
@@ -77,7 +78,7 @@ function Ecommerce() {
               <div className='mt-5'>
                 <SparkLine
                   color='black'
-                  currentColor='blue'
+                  currentColor={currentColor}
                   id='line-sparkline'
                   type='Line'
                   height='80px'
@@ -88,7 +89,7 @@ function Ecommerce() {
               <div className='mt-10'>
                 <Button
                   color='white'
-                  bgColor='blue'
+                  bgColor={currentColor}
                   text='Download Report'
                   borderRadius='10px'
                 />
